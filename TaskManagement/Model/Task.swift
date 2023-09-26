f0714ce92873f35200a166a6af55c076bd10b085
Jacek Kosinski <kashiash@gmail.com>
2023-09-26 10:36:21 +0200

    Task

---
+++ TaskManagement/Model/Task.swift
@@ -0,0 +1,18 @@
+//
+//  Task.swift
+//  TaskManagement
+//
+//  Created by Jacek Kosinski U on 26/09/2023.
+//
+
+import SwiftUI
+
+struct Task: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Task()
+}
